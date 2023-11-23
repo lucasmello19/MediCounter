bd2b9183375255f127507f08fc577b1b656a9f59
Lucas Mello <lucasmello@MacBook-Pro-de-user.local>
2023-11-23 02:51:28 -0300

    update form register layout

--- MediCounter/FormRegister/FormRegisterViewController.swift
+++ MediCounter/FormRegister/FormRegisterViewController.swift
@@ -7,11 +7,15 @@ import UIKit
 
 class FormRegisterViewController: UIViewController {
     var hoursCount: [String] = [String()]
+    
     @IBOutlet weak var tableView: AutoSizingTableView!
     @IBOutlet weak var scrollView: UIScrollView!
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.isEditing = true
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         self.tableView.estimatedRowHeight = 88.0
         self.tableView.rowHeight = UITableView.automaticDimension
@@ -20,15 +24,20 @@ class FormRegisterViewController: UIViewController {
         self.tableView.isScrollEnabled = false
         NotificationCenter.default.addObserver(self, selector: #selector(tecladoApareceu(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(tecladoDesapareceu(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tocouFora))
-        tapGesture.cancelsTouchesInView = false
-        view.addGestureRecognizer(tapGesture)
+        //        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tocouFora))
+        //        tapGesture.cancelsTouchesInView = false
+        //        view.addGestureRecognizer(tapGesture)
+        
+        setupHoursPicker()
+    }
+    
+    func setupHoursPicker() {
     }
     
     @objc func tocouFora() {
-        // Este método será chamado quando tocar fora do campo de texto
         view.endEditing(true)
     }
+    
     @objc func tecladoApareceu(_ notification: Notification) {
         if let tecladoSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
             scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: tecladoSize.height, right: 0)
@@ -38,7 +47,7 @@ class FormRegisterViewController: UIViewController {
     @objc func tecladoDesapareceu(_ notification: Notification) {
         scrollView.contentInset = .zero
     }
-
+    
     @IBAction func onClickAdd(_ sender: Any) {
         hoursCount.append(String())
         tableView.reloadData()
@@ -53,22 +62,37 @@ extension FormRegisterViewController: UITableViewDataSource, UITableViewDelegate
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cellIdentifier = "MediHoursCountCell"
         if let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MediHoursCountCell {
-            cell.setupCell(index: indexPath.row + 1)
+            
+            cell.setupCell(idx: indexPath.row)
+            
             return cell
         } else {
             return UITableViewCell()
         }
     }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (action, view, completionHandler) in
+            // Adicione aqui a lógica para a ação de exclusão
+            print("Delete action triggered for row \(indexPath.row)")
+            completionHandler(true)
+        }
+        
+        let swipeConfiguration = UISwipeActionsConfiguration(actions: [deleteAction])
+        return swipeConfiguration
+    }
+    
+    
 }
 
 final public class AutoSizingTableView: UITableView {
-
+    
     public override var contentSize: CGSize {
         didSet {
             invalidateIntrinsicContentSize()
         }
     }
-
+    
     public override var intrinsicContentSize: CGSize {
         return CGSize(width: UIView.noIntrinsicMetric, height: contentSize.height)
     }
--- MediCounter/FormRegister/MediHoursCountCell.swift
+++ MediCounter/FormRegister/MediHoursCountCell.swift
@@ -5,23 +5,105 @@
 
 import UIKit
 
-class MediHoursCountCell: UITableViewCell {
-
-    @IBOutlet weak var idxLabel: UILabel!
+class MediHoursCountCell: UITableViewCell, UIPickerViewDelegate, UIPickerViewDataSource {
+    @IBOutlet weak var txtFieldHours: UITextField!
+    @IBOutlet weak var txtAmount: UITextField!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    var idx = 0
+    
+    let amountPickerView = UIPickerView()
+    let amounts = Array([1,2,3])
+
+    let hourPickerView = UIPickerView()
+    let hours = Array(0...23)
+    
+    let minutePickerView = UIPickerView()
+    let minutes = Array(0...59)
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        if pickerView == amountPickerView {
+            return 1
+        } else {
+            return 2
+        }
     }
     
-    func setupCell(index: Int) {
-        idxLabel.text = "\(index)-"
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView == amountPickerView {
+            return amounts.count
+        } else {
+            if component == 0 {
+                return hours.count
+            } else {
+                return minutes.count
+            }
+        }
     }
     
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView == amountPickerView {
+            return "\(amounts[row]) unidade(s)"
+        } else {
+            
+            if component == 0 {
+                return "\(hours[row])h"
+            } else {
+                return "\(minutes[row])min"
+            }
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView == amountPickerView {
+            txtAmount.text = "\(amounts[row])"
+        } else {
+            let selectedHour = hours[hourPickerView.selectedRow(inComponent: 0)]
+            let selectedMinute = minutes[minutePickerView.selectedRow(inComponent: 1)]
+            txtFieldHours.text = String(format: "%02d:%02d", selectedHour, selectedMinute)
+        }
+    }
+    
+    func setupCell(idx: Int) {
+        self.idx = idx
+
+        hourPickerView.delegate = self
+        hourPickerView.dataSource = self
+        
+        minutePickerView.delegate = self
+        minutePickerView.dataSource = self
+
+        
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(setupHours))
+        toolbar.setItems([doneButton], animated: false)
+
+        txtFieldHours.inputView = hourPickerView
+        txtFieldHours.inputAccessoryView = toolbar
+        
+        amountPickerView.delegate = self
+        amountPickerView.dataSource = self
+
+        let toolbarAmount = UIToolbar()
+        toolbarAmount.sizeToFit()
+        
+        let doneButtonAmount = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(setupAmount))
+        toolbarAmount.setItems([doneButtonAmount], animated: false)
+
+        txtAmount.inputView = amountPickerView
+        txtAmount.inputAccessoryView = toolbarAmount
+    }
+    
+    @objc func setupHours() {
+        let selectedHour = hours[hourPickerView.selectedRow(inComponent: 0)]
+        let selectedMinute = minutes[minutePickerView.selectedRow(inComponent: 1)]
+        txtFieldHours.text = String(format: "%02d:%02d", selectedHour, selectedMinute)
+        txtFieldHours.resignFirstResponder()
+    }
+    
+    @objc func setupAmount() {
+        txtAmount.text = "\(amounts[amountPickerView.selectedRow(inComponent: 0)])"
+        txtAmount.resignFirstResponder()
+    }
 }
